aae7f8ee478f7eb5cb3c71a8a8df5986f34db459
idaho <664101+idaho@users.noreply.github.com>
2021-08-31 21:33:29 +0200

    fix: move to typescript

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  extends: 'idaho1980',
-  env: {
-    es6: true,
-    node: true
-  }
-};
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+let rulesDirectories: string[] = [];
+
+export default {
+  get rules (): Record<string, any> {
+    if (rulesDirectories.length === 0) {
+      throw new Error('no custom local rule directory defined');
+    }
+
+    const rules = rulesDirectories.
+      flatMap((directory): any =>
+      // eslint-disable-next-line global-require, @typescript-eslint/no-require-imports
+        require(directory));
+
+    if (rules.length === 0) {
+      throw new Error('no rules found');
+    }
+
+    return rules;
+  },
+
+  addDirectories (addDirectories: string[]): void {
+    rulesDirectories = [
+      ...rulesDirectories,
+      ...addDirectories
+    ];
+  }
+};
